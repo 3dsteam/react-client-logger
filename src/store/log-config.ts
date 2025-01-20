5b2cd82c021dececeb63c2d62ac58b05d3561442
Lorenzo Bonatti <lorenzo.bonatti@3ds.team>
2025-01-20 10:40:29 +0100

    feat: add configurations localStorage saving

--- src/store/log-config.ts
+++ src/store/log-config.ts
@@ -1,15 +1,21 @@
 import { create } from "zustand";
 import { ELogLevel, ILogConfig } from "../models";
+import { persist } from "zustand/middleware";
 
 interface IActions {
     updateLogConfig: (config: Partial<ILogConfig>) => void;
 }
 
-export const useLogConfigStore = create<ILogConfig & IActions>((set) => ({
-    logLevel: ELogLevel.ERROR,
-    ignoreLevels: ELogLevel.DEBUG,
-    breadcrumbs: 50,
+export const useLogConfigStore = create(
+    persist<ILogConfig & IActions>(
+        (set) => ({
+            logLevel: ELogLevel.ERROR,
+            ignoreLevels: ELogLevel.DEBUG,
+            breadcrumbs: 50,
 
-    // Actions
-    updateLogConfig: (config) => set(config),
-}));
+            // Actions
+            updateLogConfig: (config) => set(config),
+        }),
+        { name: "client-logger-log-config" },
+    ),
+);
--- src/store/sync-config.ts
+++ src/store/sync-config.ts
@@ -1,16 +1,22 @@
 import { create } from "zustand";
 import { ISyncerConfig } from "../models";
+import { persist } from "zustand/middleware";
 
 interface IActions {
     updateSyncConfig: (config: Partial<ISyncerConfig>) => void;
 }
 
-export const userSyncConfigStore = create<ISyncerConfig & IActions>((set) => ({
-    endpoint: "/api/logs",
-    headers: { "Content-Type": "application/json" },
-    chunk: 10,
-    interval: 60000,
+export const userSyncConfigStore = create(
+    persist<ISyncerConfig & IActions>(
+        (set) => ({
+            endpoint: "/api/logs",
+            headers: { "Content-Type": "application/json" },
+            chunk: 10,
+            interval: 60000,
 
-    // Actions
-    updateSyncConfig: (config) => set(config),
-}));
+            // Actions
+            updateSyncConfig: (config) => set(config),
+        }),
+        { name: "client-logger-sync-config" },
+    ),
+);
